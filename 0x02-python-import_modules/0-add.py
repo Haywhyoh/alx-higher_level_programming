17ef010ce3908908c5e9678a83f6c31f3b0b3b55
Haywhyoh <adefeyisayo998@gmail.com>
2022-06-02 16:54:54 -0700

    ran module as a script

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import add_0 as add_num
-
-a = 1
-b = 2
-print("{} + {} = {}".format(a, b, add_num.add(a, b)))
+if __name__ == "__main__"
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add_num.add(a, b)))
