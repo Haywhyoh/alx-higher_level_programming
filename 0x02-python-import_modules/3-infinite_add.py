e01c216fde88832a9ce2b1acfb9a9c8f418e5559
Haywhyoh <adefeyisayo998@gmail.com>
2022-06-02 17:36:11 -0700

    fixed error

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,7 +5,7 @@ def main():
     argc = len(sys.argv)
     sum = 0
     for i in range(1, argc):
-        sum = sum + argv[i]
+        sum += int(sys.argv[i])
     print(sum)
 
 if __name__ == "__main__":
