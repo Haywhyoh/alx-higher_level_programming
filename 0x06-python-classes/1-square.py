4252bffb2b9cef25f8ae40674dff443863b13700
Haywhyoh <adefeyisayo998@gmail.com>
2022-06-22 06:16:35 -0700

    fixed pepstyle

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,8 +3,7 @@ class Square:
     """ Class that declares a square"""
 
     def __init__(self, size):
-        """
-        Initialize the instance of a square
+        """Initialize the instance of a square
 
         Args:
             param1 (size): size of a square
