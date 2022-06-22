acf2f780791c5b1b98b93a1bc23bc29ff1d5591f
Haywhyoh <adefeyisayo998@gmail.com>
2022-06-22 06:09:13 -0700

    did task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+class Square:
+    """ Class that declares a square"""
+
+
+    def __init__(self, size):
+        """
+        Initialize the instance of a square
+
+        Args:
+            param1 (size): size of a square
+        """
+        self.__size = size
