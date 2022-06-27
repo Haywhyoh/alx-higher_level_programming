c9771dcd86bca98445644561e3f26ed270042595
Haywhyoh <adefeyisayo998@gmail.com>
2022-06-27 02:46:08 -0700

    created tsk 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""
+    A square module
+"""
+
+
+Class Square:
+    """
+        A class to define square properties
+    """
+
+    def __init(self, size=0):
+        """
+            Method to initialize size
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """
+            method to get the value
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+            Method to set variable
+        """
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        elif not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        else:
+            self.__size = value
+
+    def area(self):
+        """
+        Method to calculate area
+        """
+        return (self.__size ** 2)
+
+    def my_print(self, size=0):
+        """
+            Method to print # in square
+            according to the size value
+        """
+        if self.__size = 0:
+            print()
+        for width in range(self.__size):
+            for length in range(self.__size):
+                print('#', end='')
+            print()
