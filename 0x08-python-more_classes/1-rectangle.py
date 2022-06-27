3a050a8de7bf56c161d855d7d8ddc987564e9ecd
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-06-27 18:00:24 +0100

    created the init method

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This is a Rectangle class"""
+
+
+class Rectangle:
+    """Class to create a Rectangle object"""
+
+    def __init__(self, width=0, height=0):
+        """Initialize the rectangle object"""
+        self.width = width
+        self.height = height
+    
+    
