1c84d79c5e673ba777b83dc9dfebd7a9e83acf5d
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-06-28 08:45:04 +0100

    did task 100 again

--- 0x05-python-exceptions/100-safe_print_integer-err.py
+++ 0x05-python-exceptions/100-safe_print_integer-err.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-import sys
 
 
 def safe_print_integer_err(value):
-    try:
-        print('{:d}'.format(value))
-    except Exception as error:
-        print('Exception: {}'.format(error), file=sys.stderr)
-        return False
 
-    return True
+    import sys
+
+    try:
+        print("{:d}".format(value))
+        return(True)
+    except (ValueError, TypeError) as err:
+        sys.stderr.write("Exception: {}\n".format(err))
+        return(False)
