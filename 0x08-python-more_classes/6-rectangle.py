2b645a600be4c1993ddd9832aa90cae19b6ccc0b
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-06-29 18:50:46 +0100

    added an instance decrement to the del functiion

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -67,5 +67,5 @@ class Rectangle:
 
     def __del__(self):
         """Print a message for every deletion of a Rectangle."""
-        Rectangle.number_of_instances += 1
+        Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
