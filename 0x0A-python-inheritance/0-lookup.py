a6f3234be841c1828a8c9da07faa7d6b2a8c6ee6
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-07-04 14:27:04 +0100

    added the shebang

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def lookup(obj):
+    """A function that returns the list of available attributes and methods of an object.
+    
+        Args:
+            obj: The object class parameter
+            
+        Returns: List of the method and attributes."""
+    return(dir(obj))
