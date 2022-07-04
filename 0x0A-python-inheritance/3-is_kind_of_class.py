eb98f9b7a8037d25139bb5d76db9af69edcf7c75
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-07-04 22:56:02 +0100

    did task 2

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""The modeule check for the instance of an object"""
+
+
+def is_kind_of_class(obj, a_class):
+    """This function that returns True if the object is an instance of,
+    or if the object is an instance of a class that inherited from,
+     the specified class.  
+
+     Args:
+        obj: The object class.
+        a_class: the class to be tested.
+
+    Returns: True id the object is an instance or if the object
+    is an instance of a class that inherited from."""
+    return isinstance(obj, a_class)
