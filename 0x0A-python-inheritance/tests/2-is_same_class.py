bd6d8aef0016c0475de3f4113868afc859723a3d
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-07-04 22:34:09 +0100

    did the task

---
+++ 0x0A-python-inheritance/tests/2-is_same_class.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""The module or an obj class"""
+
+
+"""The class creates an instance obj."""
+
+def is_same_class(obj, a_class):
+    """Function checks if the object is exactly 
+    an instance of the specified class.
+
+    Args:
+        a_class: the instance to be checked
+        object: the object class
+
+    Returns: True if the is object is an instance,
+            and false otherwise
+    """
+    if (isinstance(a_class, obj)):
+        return True
+    else:
+        return False
