36e96fbc9fe254f1c81e4defef31a180f92d1d73
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-07-04 22:59:12 +0100

    removed trailing line

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -5,7 +5,7 @@
 def is_kind_of_class(obj, a_class):
     """This function that returns True if the object is an instance of,
     or if the object is an instance of a class that inherited from,
-     the specified class.  
+     the specified class.
 
      Args:
         obj: The object class.
