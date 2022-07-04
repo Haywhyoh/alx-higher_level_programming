309eeaaa2c47dbf627659feede2ff24eeb1ee01f
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-07-04 23:02:53 +0100

    typo error

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -2,7 +2,7 @@
 """The modeule check for the instance of an object"""
 
 
-def is_kind_of_class(obj, a_class):
+def inherits_from(obj, a_class):
     """This function that returns True if the object is an instance of,
     or if the object is an instance of a class that inherited from,
      the specified class.
