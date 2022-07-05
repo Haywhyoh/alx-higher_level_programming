c3a9b0dd2e2cd5079bbba65f49e5e264a708bb12
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-07-05 10:16:56 +0100

    created the file for task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Input and output python module."""
+
+
+def append_write(filename="", text=""):
+    """ Function that appends a string at the end of a text file (UTF8)
+        and returns the number of characters added.
+
+        Args:
+            filename: the name of the file to be read.
+            text: the text to be written into the file.
+
+        Returns:
+            numbers of characters written."""
