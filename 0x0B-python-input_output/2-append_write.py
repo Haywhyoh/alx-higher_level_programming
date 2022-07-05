ce34cbd63832a7a4da5f842b9b6b301157540145
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-07-05 10:06:43 +0100

    created the 2-append_write file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Input and output python module."""
+
+
+def write_file(filename="", text=""):
+    """ Function that writes a string to a text file (UTF8)
+        and returns the number of characters written:
+
+        Args:
+            filename: the name of the file to be read.
+            text: the text to be written into the file.
+
+        Returns:
+            numbers of characters written."""
