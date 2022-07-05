53f51bb571e10f050ef6f79f1647869aaa15e57a
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-07-05 22:46:06 +0100

    created the python file

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Input and output python module."""
+import json
+
+
+def load_from_json_file(filename):
+    """ Function that creates an Object from a “JSON file”
+
+        Args:
+            my_obj: the object to be written into a file
+            filename: the file the JSON is written into.
+
+        Returns:
+            an object."""
+    with open(filename, encoding='utf-8') as my_file:
+       return (my_file.load(filename))
