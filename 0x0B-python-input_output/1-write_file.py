190bb48a26acae4b9bece431173980fff4b91cb2
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-07-05 10:01:20 +0100

    did task 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Input and output python module."""
+
+
+def write_file(filename="", text=""):
+    """ Function that writes a string to a text file (UTF8)
+        and returns the number of characters written:
+
+        Args:
+            filename: the name of the file to be read.
+            text: the text to be written into the file.
+
+        Returns:
+            numbers of characters written."""
+    with open(filename, mode='w', encoding='utf-8') as file:
+        content = file.write(text)
+        return content
