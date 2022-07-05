e29eeec943d4d4afb1f55ce584b15a8754be0b1d
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-07-05 10:51:17 +0100

    created the task file

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import json
+"""Input and output python module."""
+
+
+def from_json_string(my_str):
+    """ Function that returns an object (Python data structure)
+        represented by a JSON string.
+
+        Args:
+            my_str: the string to be converted
+
+        Returns:
+            an object (Python data structure."""
