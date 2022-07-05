5873c07d73c32cf7f2194671495979a9d67fb7fd
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-07-05 10:58:19 +0100

    did the task 5

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -3,12 +3,15 @@ import json
 """Input and output python module."""
 
 
-def from_json_string(my_str):
-    """ Function that returns an object (Python data structure)
-        represented by a JSON string.
+def save_to_json_file(my_obj, filename):
+    """ Function that writes an Object to a text file,
+        using a JSON representation.
 
         Args:
-            my_str: the string to be converted
+            my_obj: the object to be written into a file
+            filename: the file the JSON is written into.
 
         Returns:
             an object (Python data structure."""
+    with open(filename, mode='w', encoding='utf-8') as my_file:
+        json.dump(my_obj, my_file)
