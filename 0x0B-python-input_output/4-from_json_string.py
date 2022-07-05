b05b12d2f8db33d61c99779d3868443dd1e94d99
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-07-05 10:35:34 +0100

    created task 4 file

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import json
+"""Input and output python module."""
+
+
+def to_json_string(my_obj):
+    """ Function that returns the JSON representation
+        of an object (string):
+
+        Args:
+            my_obj: the object to be converted
+
+        Returns:
+            JSON representation of the my_obj."""
