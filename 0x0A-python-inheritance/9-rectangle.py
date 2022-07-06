cff89023e912984ec79e51c7ba3ab6cdfb136464
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-07-06 16:41:49 +0100

    did one magic

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,26 +1,41 @@
 #!/usr/bin/python3
-"""A module for Rectangular operations"""
+"""Module 9-rectangle.
+Creates a Rectangle class.
+"""
+
+
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """A class to create and manipulate rectangle object"""
+    """Represents a rectangle.
+    Private instance attributes:
+        - width
+        - height
+    Public method area().
+    Inherits from BaseGeometry.
+    """
 
     def __init__(self, width, height):
-        """initializer
-        Args
-           width
-           height
+        """Initializes an instance.
+        Args:
+            - width: width of the rectangle
+            - heigth: height of the rectangle
         """
-        self.integer_validator('width', width)
-        self.__width__ = width
-        self.integer_validator('height', height)
-        self.__height__ = height
 
-    def area(self):
-        """area of rectangle"""
-        return(self.__height * self.__width)
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
 
     def __str__(self):
-        """Return the print() and str() representation of a Rectangle."""
+        """Returns a formatted string."""
+
         return str("[Rectangle] {}/{}".format(self.__width, self.__height))
+
+    def area(self):
+        """Computes the area of the Rectangle instance.
+        Overwrites the area() method from BaseGeometry.
+        """
+
+        return self.__width * self.__height
