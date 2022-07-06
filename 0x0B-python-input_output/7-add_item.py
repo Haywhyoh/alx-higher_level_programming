22fbe7de13692b5cf22ff27cb1bbb879d363860a
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-07-06 05:20:09 +0100

    pycode fix

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Input and output python module."""
 import sys
-import json
 
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_from_json_file').save_from_json_file
