7e2d92d846266519f8c701d2ab86667cd24617a8
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-07-06 11:50:47 +0100

    pycodestyle troubles

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""The module for the BaseGeometry Class"""
+
+
+class BaseGeometry:
+
+    """
+    a class for BaseGeometry.
+    """
+
+    def area(self):
+        """Function to raise an exception"""
+        raise Exception('area() is not implemented')
