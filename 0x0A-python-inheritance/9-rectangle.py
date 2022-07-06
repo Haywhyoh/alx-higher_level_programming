8d79749608cc1694eb854f0b0ad545f285a25f7b
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-07-06 16:37:27 +0100

    another dtring thing

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -23,4 +23,6 @@ class Rectangle(BaseGeometry):
 
     def __str__(self):
         """Return the print() and str() representation of a Rectangle."""
-        return str("[Rectangle] {}/{}".format(self.__width, self.__height))
+        string = "[" + str(self.__class__.__name__) + "] "
+        string += str(self.__width) + "/" + str(self.__height)
+        return string
