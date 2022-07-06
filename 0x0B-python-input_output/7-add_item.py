8f50aec0eca537daa32313cb3f46d68142b375ff
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-07-06 05:18:38 +0100

    did task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Input and output python module."""
+import sys
+import json
+
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_from_json_file').save_from_json_file
+
+try:
+    item_list = load_from_json_file('add_item.json')
+except FileNotFoundError:
+    item_list = []
+input_arguments = sys.argv[1:]
+item_list.extend(input_arguments)
+save_to_json_file(item_list, 'add_item.json')
