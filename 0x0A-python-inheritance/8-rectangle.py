f8ef522c8510182847843dcb6bc5436ff9d2929f
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-07-06 15:01:49 +0100

    removed trailing stop

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -6,7 +6,7 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 class Rectangle(BaseGeometry):
     """A class to create and manipulate rectangle object"""
 
-    def __init__(self, width, height): 
+    def __init__(self, width, height):
         """initializer
         Args
            width
