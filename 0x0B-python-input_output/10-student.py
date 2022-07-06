07e6ef0afa0018485150a418ffcd1ab51a6bc8ae
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-07-06 10:48:04 +0100

    pushed it

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -17,11 +17,12 @@ class Student:
         self.age = age
 
     def to_json(self, attrs=None):
-        """Get a dictionary representation of the Student."""
-        if not isinstance(self.attrs, list):
-            return self.__dict__
-
+        """retrieves dictionary representation of instance"""
+        if attrs is None:
+            return (self.__dict__)
+        if not isinstance(attrs, list):
+            return (self.__dict__)
         if not (isinstance(attrs[elem], str) for elem in attrs):
-            return self.__dict__
-        return({key : value for key, value in self.__dict__.item()
-                if key in attrs})
+            return (self.__dict__)
+        return ({key: value for key, value in self.__dict__.items()
+                 if key in attrs})
