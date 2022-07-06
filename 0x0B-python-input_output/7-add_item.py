9250a37d8d3ae72ced202a5aabb6ada9cc0746c4
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-07-06 05:25:54 +0100

    some changes

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """Input and output python module."""
 import sys
-
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('5-save_from_json_file').save_from_json_file
+if __name__ == "__main__":
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
+    save_to_json_file = __import__('5-save_from_json_file').save_from_json_file
 
 try:
     item_list = load_from_json_file('add_item.json')
