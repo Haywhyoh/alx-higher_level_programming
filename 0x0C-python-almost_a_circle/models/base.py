0950757993d096199a33df88c8a9b143ff95d2e3
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-07-11 07:10:26 +0100

    did base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,10 +1,21 @@
 #!usr/bin/python3
+""" A python module to do something"""
+
+
 class Base:
+    """A base class for all the other classes
+        in this project.
+    """
+
     __nb_objects = 0
 
     def __init__(self, id=None):
-        if id != None:
+        """Initializrion function 
+        
+            Args:
+                id: the in to be returned."""
+        if id == None:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        else:
             self.id = id
-            
-        __nb_objects += 1
-        self.id = id
