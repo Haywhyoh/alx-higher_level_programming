0095f8c97fe1704b50b16baf213501f32050821e
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-08-10 06:23:05 +0100

    anothern adddition

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,5 +1,6 @@
--- some operations
-SELECT `score` COUNT(*) as 'numbers'
+-- Lists the number of records with the same score in the table second_table.
+-- Records are ordered by descending count.
+SELECT `score`, COUNT(*) AS `number`
 FROM `second_table`
-GROUP BY (`scores`)
+GROUP BY `score`
 ORDER BY `number` DESC;
