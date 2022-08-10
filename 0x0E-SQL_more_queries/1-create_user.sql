392ff54867b9227ffad2f55cb0ea2240b2fbba8b
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-08-10 14:18:15 +0100

    some '' problem

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -3,5 +3,5 @@
 CREATE USER IF NOT EXISTS user_0d_1@localhost
 ALTER USER  'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 -- grants privileges
-GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@localhost;
 FLUSH PRIVILEGES
