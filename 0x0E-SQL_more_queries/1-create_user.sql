b5d0f74ea23bae43017493bc239712d9eec3c35c
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-08-10 14:19:47 +0100

    added ;

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -4,4 +4,4 @@ CREATE USER IF NOT EXISTS user_0d_1@localhost;
 ALTER USER  'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 -- grants privileges
 GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@localhost;
-FLUSH PRIVILEGES
+FLUSH PRIVILEGES;
