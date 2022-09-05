21c464ebea5ca621b71ee828cbc43132352bb021
ayomide <adefeyisayo998@gmail.com>
2022-09-05 16:52:16 +0000

    tired of semistandard

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
-const myVar = process.argv[1];
+const myVar = process.argv[2];
 const argLength = process.argv.length;
 if (argLength < 3) {
-  console.log ('No argument');
-else {
-  console.log (myVar)
+  console.log('No argument');
+} else {
+  console.log(myVar);
+}
