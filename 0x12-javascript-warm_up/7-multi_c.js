84c0a41be86458633a287b590437d48d7ec8200a
ayomide <adefeyisayo998@gmail.com>
2022-09-05 18:39:25 +0000

    changed <= ffor the loop to <

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,7 +2,7 @@
 if (isNaN(parseInt(process.argv[2]))) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i <= process.argv[2]; i++) {
+  for (let i = 0; i < process.argv[2]; i++) {
     console.log('C is fun');
   }
 }
