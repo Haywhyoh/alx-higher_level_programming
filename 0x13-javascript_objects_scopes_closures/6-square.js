cd28206e3498c14f59fd0d8a21cbbc19e77f2c47
ayomide <adefeyisayo998@gmail.com>
2022-09-07 09:02:03 +0000

    semistandard fix

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
-module.exports = class Square extends requestAnimationFrame('./5-square.js') {
-  charPrint(c) {
+module.exports = class Square extends require('./5-square.js') {
+  charPrint (c) {
     if (c === undefined) {
-        this.print();
+      this.print();
     } else {
-        for (let i = 0; i < this.height; i++) {
-          console.log(c.repeat(this.width))
-        }
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
     }
   }
 };
