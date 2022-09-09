968b89f9dcc89e6a35d475579d43727e3c56f511
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-09-09 10:15:17 +0100

    did task 1

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Lists all states from the database hbtn_0e_0_usa
+"""
+import sys
+import MySQLdb
+username = sys.argv[1]
+password = sys.argv[2]
+database = sys.argv[3]
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost', user=username, passwd=password, port=3306, db=database)
+    cur = db.cursor()
+    cur.execute("USE hbtn_0e_0_usa, 'SELECT * FROM states, ORDER BY id ASC")
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+
+    db.close()
