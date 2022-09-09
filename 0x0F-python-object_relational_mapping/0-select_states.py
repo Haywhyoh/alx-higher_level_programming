f99a12d7d03be2ac8d85bdcfece040467eac0862
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-09-09 13:38:12 +0100

    pycode fix

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,11 +5,12 @@ Lists all states from the database hbtn_0e_0_usa
 import sys
 import MySQLdb
 username = sys.argv[1]
-password = sys.argv[2]
+pass_w = sys.argv[2]
 database = sys.argv[3]
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host='localhost', user=username, passwd=password, port=3306, db=database)
+    db = MySQLdb.connect(host='localhost', user=username,
+        passwd=pass_w, db=database)
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     states = cur.fetchall()
