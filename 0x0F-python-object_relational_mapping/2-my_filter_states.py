050129d3f0279cb55024d843164b05aa674b2ef2
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-09-12 14:47:11 +0100

    changed ordering

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,7 +8,7 @@ import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELCET * FROM states WHERE name = '{%s}' ORDER BY id".format(sys.argv[4]))
+    cur.execute("SELCET * FROM states WHERE name = '{%s}' ORDER BY id ASC".format(sys.argv[4]))
 
     for state in cur.fetchall():
         if state[1] == sys.argv[4]:
