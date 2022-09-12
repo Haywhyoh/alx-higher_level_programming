9c24fa2a9eb6aafa869adf217c8b9664f27fb886
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-09-12 12:17:59 +0100

    document module

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """
-Lists all states from the database hbtn_0e_0_usa
+Lists all states from the database hbtn_0e_0_usa.
+Usage: ./0-select_states.py <mysql username> \
+                            <mysql password> \
+                             <database name>
 """
 import sys
 import MySQLdb
@@ -16,5 +19,4 @@ if __name__ == "__main__":
     states = cur.fetchall()
     for state in states:
         print(state)
-
     db.close()
