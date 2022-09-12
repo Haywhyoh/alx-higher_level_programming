6e21918719d023f4fd22e9a9a8476e682c493b9f
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-09-12 21:24:53 +0100

    added states.id

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute('SELECT cities.id, cities.name, states.name \
-        FROM cities INNER JOIN states ON state.id = cities.state_id\
+        FROM cities INNER JOIN states ON states.id = cities.state_id\
         ORDER BY cities.id ASC')
     for state in cur.fetchall():
         print(state)
