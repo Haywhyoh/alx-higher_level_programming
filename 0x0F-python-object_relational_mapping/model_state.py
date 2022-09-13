611491c0d89627526da016189f525ac74f349712
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-09-13 10:49:50 +0100

    changes id to name

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -3,12 +3,13 @@
 a python file that contains the class definition of a State
 and an instance Base = declarative_base():
 """
-from sqlalchemy.orm import declarative_base
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base
+
 Base = declarative_base()
 
 
 class State(Base):
-    __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False, unique=True)
-    id = Column(String(128), nullable=False )
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
