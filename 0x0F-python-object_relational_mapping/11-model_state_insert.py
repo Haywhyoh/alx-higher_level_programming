53600f11d7eaf2c7146aac1c3d9ba2bfe2d41c87
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-09-14 00:03:44 +0100

    just finished task 11

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This is something I can handle
+but they don't believe in me
+"""
+import sys
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from model_state import State, Base
+
+engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                       .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                       pool_pre_ping=True)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+louisiana = State(name="Louisiana")
+session.add(louisiana)
+session.commit()
+print(louisiana.id)
