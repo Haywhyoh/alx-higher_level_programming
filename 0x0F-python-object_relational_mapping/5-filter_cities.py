e9e702c3434450a87f5c862079b25e95623617dd
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-09-13 09:25:07 +0100

    radical changes again

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -11,9 +11,7 @@ if __name__ == '__main__':
 
     cur = db.cursor()
     cur.execute("SELECT cities.id, cities.name, states.name \
-    FROM cities JOIN states ON cities.state_id = states.id \
-    WHERE states.name = '{}';".format(sys.argv[4]))
+        FROM cities JOIN states ON cities.states_id = states.id WHERE \
+        states.name = '{}';".format(sys.argv[4]))
     states = cur.fetchall()
-
     print(", ".join([state[1] for state in states]))
-    
