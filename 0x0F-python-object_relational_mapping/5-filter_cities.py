4c06e4a060239c7211ab545d9a507304ab444d21
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-09-13 09:19:41 +0100

    i'm tired of updates

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -2,14 +2,14 @@
 """
 List all cities of a state
 """
-from sys import argv
+import sys
 import MySQLdb
 
 if __name__ == '__main__':
-    db = MySQLdb.connect(user=argv[1], passwd=argv[2],
-                         db=argv[3], port=3306)
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
 
     cur = db.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.states_id = states.id WHERE states.name = '{}';".format(argv[4]))
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.states_id = states.id WHERE states.name = '{}';".format(sys.argv[4]))
     states = cur.fetchall()
     print(", ".join([state[1] for state in states]))
