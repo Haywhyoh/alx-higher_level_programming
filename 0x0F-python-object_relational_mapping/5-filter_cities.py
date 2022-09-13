6d029a5a3340e7ea7762ccd68c2a1c07f01661e7
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-09-13 07:44:42 +0100

    changed to cities.state_id = states.id

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor
     cur.execute("SELECT cities.id, cities.name, states.names FROM cities\
-                JOIN states ON states.id = cities.state_id WHERE states.name = '{}'", (argv[4],))
+                INNER JOIN states ON cities.state_id = states.id WHERE states.name = '{}'", (argv[4],))
 
     (", ".join ([state[1] for state in cur.fetchall()]))
 
