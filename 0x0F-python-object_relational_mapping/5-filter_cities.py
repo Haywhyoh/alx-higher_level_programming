06df5c98793de6e3b897263715ef566858cd1230
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-09-13 07:43:21 +0100

    change inner join to join

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor
     cur.execute("SELECT cities.id, cities.name, states.names FROM cities\
-                INNER JOIN states ON states.id = cities.state_id WHERE states.name = %s", (argv[4],))
+                JOIN states ON states.id = cities.state_id WHERE states.name = '{}'", (argv[4],))
 
     (", ".join ([state[1] for state in cur.fetchall()]))
 
