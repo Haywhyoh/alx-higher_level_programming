4b5219305d84c598ecb299bb10c5d7c2c0badf98
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-09-24 19:18:54 +0100

    grep -i added

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Script that shpws the Content-Length from a HTTP request
-curl -sI  "$1" | grep "Content-Length" | cut  -f2 -d ":"
+curl -sI  "$1" | grep -i "Content-Length" | cut  -f2 -d ":"
