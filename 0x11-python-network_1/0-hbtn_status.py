8dfe379734c76f37e03a377b5f59fcbce3532b18
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-09-27 08:33:29 +0100

    fixed pycodestyle error

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,7 +4,7 @@ import urllib.request
 req = urllib.request.Request('http://alx-intranet.hbtn.io/status')
 with urllib.request.urlopen(req) as response:
     the_page = response.read()
-print ('Body response: $:')
+print('Body response: $:')
 print("\t- type {}".format(type(the_page)))
 print("\t- content {}".format(the_page))
 print("\t- utf8 content {}".format(str(the_page)))
