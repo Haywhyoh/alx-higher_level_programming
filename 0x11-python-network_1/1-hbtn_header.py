3a7ccd6a82afc9d6e1954ffe1f68fd4e2558f445
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-09-27 09:35:16 +0100

    task 1 completed

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL, sends a request and displays
+the value of the X-Request-Id variable found in the header
+"""
+import sys
+import urllib.request
+
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1]) as res:
+        html = res.info()
+print(html.get('X-Request-Id'))
