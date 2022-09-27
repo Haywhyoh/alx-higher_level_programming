ea4f73f7c511a8660c3209ad733efd05b431e4f7
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-09-27 15:55:37 +0100

    i dey go small

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""""""
+import urllib.request
+import urllib.error
+import sys
+
+if __name__ == "__main__":
+    #we try the request
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as response:
+            body = response.read().decode('utf-8')
+            print(body)
+    except urllib.error.urllib.error.HTTPError as e:
+        code = e.code
+        print("Error code:{:d}".format(code))
