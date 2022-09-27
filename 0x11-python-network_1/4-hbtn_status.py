5a741e2812d02cbc84e2ed855daacecafe5a7654
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-09-27 22:17:29 +0100

    i tried something else

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-'''fetches status of https://intranet.hbtn.io/status'''
+"""
+Python script that fetches an URL with requests package
+"""
 import requests
 
-r = requests.get('https://intranet.hbtn.io/status')
-print("Body response:")
-print("\t- type: {}".format(type(r.text)))
-print("\t- content: {}".format(r.text))
+
+if __name__ == "__main__":
+    r = requests.get('https://intranet.hbtn.io/status')
+    t = r.text
+    print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(t), t))
