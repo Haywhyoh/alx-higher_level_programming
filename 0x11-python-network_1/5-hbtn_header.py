f9747f0281ebbe6150705eacbc51f4fbe74b4f6f
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-09-28 16:24:26 +0100

    added comments

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -8,5 +8,10 @@ import sys
 
 
 if __name__ == "__main__":
+    #made the request get with the url
+    #r = requests.get(sys.argv[1])
     r = requests.get(sys.argv[1])
+    # used the headers.ger to get requests
+    #req = r.headers.get('X-Request-Id')
+    #print(req)
     print(r.headers.get('X-Request-Id'))
