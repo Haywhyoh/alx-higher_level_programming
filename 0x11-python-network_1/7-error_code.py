b8ebab697453518d82013be0b0b339ecc2d1fdf2
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-09-28 16:31:15 +0100

    added the sample

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -9,10 +9,15 @@ import requests
 
 
 if __name__ == "__main__":
+    #set the url in the address
     url = sys.argv[1]
-
+    #get the request neeed with url as parameter
     r = requests.get(url)
+    #check if the status_code is 404
     if r.status_code >= 400:
+        #if r.status_code >=400
+        #print error coded
         print("Error code: {}".format(r.status_code))
     else:
+        #ifno error code print r.text
         print(r.text)
