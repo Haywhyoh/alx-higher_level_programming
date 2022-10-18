14abb109a0f6cfcda19bf7a8e3630d93e3ac4040
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-10-18 10:28:54 +0100

    did task 0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/*
+reads a file and prints the contents
+*/
+const fs = require('fs')
+const file = process.argv[1]
+fs.readFile(file, 'uf8', (err, data) => {
+  if (err) {
+    console.error(err)
+    return
+  }
+  console.log(data)
+})
