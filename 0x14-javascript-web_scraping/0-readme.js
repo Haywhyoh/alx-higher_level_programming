7316cbd8fd4d87b532378ed592fa862a09e3557c
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-10-18 10:30:52 +0100

    changed the value of the argument variable

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,7 +3,7 @@
 reads a file and prints the contents
 */
 const fs = require('fs')
-const file = process.argv[1]
+const file = process.argv[2]
 fs.readFile(file, 'uf8', (err, data) => {
   if (err) {
     console.error(err)
