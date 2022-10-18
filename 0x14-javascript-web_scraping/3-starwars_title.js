ff802d8d9125c41c8f7e083e206deeb0f585746f
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-10-18 13:22:32 +0100

    adjusted the ndpoint

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
 let id = process.argv[2];
-const url =  'https://swapi-api.hbtn.io/api/films/:' + id;
+const url =  'https://swapi-api.hbtn.io/api/films/' + id;
 
 request(url, function (error, response, body) {
   if (error) {
