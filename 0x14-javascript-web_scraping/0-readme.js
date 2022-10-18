936f301897a5d3746a0dbf4df35ef4f4b144ff5b
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-10-18 10:32:55 +0100

    some semi colon fixes

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,12 +2,12 @@
 /*
 reads a file and prints the contents
 */
-const fs = require('fs')
-const file = process.argv[2]
+const fs = require('fs');
+const file = process.argv[2];
 fs.readFile(file, 'uf8', (err, data) => {
   if (err) {
-    console.error(err)
-    return
+    console.error(err);
+  } else {
+    console.log(data);
   }
-  console.log(data)
-})
+});
