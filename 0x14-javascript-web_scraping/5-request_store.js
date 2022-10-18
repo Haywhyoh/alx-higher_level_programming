e5085b48e89906ddd60e6d34aa50eaad5e16dec0
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-10-19 00:22:54 +0100

    tried something else

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,14 +1,22 @@
 #!/usr/bin/node
-
+/*
+script that gets the contents of a webpage and stores it in a file.
+*/
 const request = require('request');
 const fs = require('fs');
-const url = process.argv[2];
-const filePath = process.argv[3];
 
-request(url, function (err, response, body) {
+let url = process.argv[2];
+let file = process.argv[3];
+
+request(url, function (err, res, body) {
   if (err) {
     console.log(err);
   } else {
-    fs.writeFile(filePath, body, 'utf-8');
+    let content = body;
+    fs.writeFile(file, content, function (err) {
+      if (err) {
+        console.log(err);
+      }
+    });
   }
 });
