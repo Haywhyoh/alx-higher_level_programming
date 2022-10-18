ec702731eeed88b6fb015565fb46abba9c34f324
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-10-19 00:14:59 +0100

    code update

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,17 +1,16 @@
 #!/usr/bin/node
-/*
-prints the title of a Star Wars movie
-the episode number matches a given integer.
-*/
-const request = require('request');
-let id = process.argv[2];
-const url =  'https://swapi-api.hbtn.io/api/films/' + id;
 
-request(url, function (error, response, body) {
-  if (error) {
-    console.log(error);
+const request = require('request');
+const episodeNum = process.argv[2];
+const API_URL = 'https://swapi-api.hbtn.io/api/films/';
+
+request(API_URL + episodeNum, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const responseJSON = JSON.parse(body);
+    console.log(responseJSON.title);
   } else {
-    let json_file = JSON.parse(body);
-    console.log(json_file.title);
+    console.log('Error code: ' + response.statusCode);
   }
 });
