45e4d8776bb1ea02bedebae23af952641ccb3a2e
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-10-18 12:37:22 +0100

    fixed the semi standard

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,11 +3,12 @@
 reads a file and prints the contents
 */
 const fs = require('fs');
-const content = process.argv[3];
-const file = process.argv[2];
 
-fs.writeFile(file, content, err => {
-    if (err) {
-        console.error(err);
-    }
+let file = process.argv[2];
+let content = process.argv[3];
+
+fs.writeFile(file, content, function (err) {
+  if (err) {
+    console.log(err);
+  }
 });
