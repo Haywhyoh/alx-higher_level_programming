03f3798187d5f3c039cedf71799cb213ef66fc87
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-10-19 00:24:05 +0100

    another attempt

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,14 +1,9 @@
 #!/usr/bin/node
-/*
-reads a file and prints the contents
-*/
+
 const fs = require('fs');
+const file = process.argv[2];
+const string = process.argv[3];
 
-let file = process.argv[2];
-let content = process.argv[3];
-
-fs.writeFile(file, content, function (err) {
-  if (err) {
-    console.log(err);
-  }
+fs.writeFile(file, string, 'utf-8', function (err) {
+  if (err) console.log(err);
 });
