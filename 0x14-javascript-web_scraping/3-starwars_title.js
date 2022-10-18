13af0adf13b274855744309c52d6c9fb44e1d1e4
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-10-18 13:21:15 +0100

    tried task 3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const request = require('request');
+let id = process.argv[2];
+const url =  'https://swapi-api.hbtn.io/api/films/:' + id;
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    let json_file = JSON.parse(body);
+    console.log(json_file.title);
+  }
+});
